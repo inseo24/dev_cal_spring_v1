a27ef6c8d731b8e9c3260ed84f3f08c778756d53
seoin <befine324@gmail.com>
2021-12-16 15:57:47 +0900

    remove : comments(주석)

--- src/main/java/com/example/ecommerce/controller/UserController.java
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -9,7 +9,6 @@ import com.example.ecommerce.model.UserEntity;
 import com.example.ecommerce.persistence.UserRepository;
 import com.example.ecommerce.security.TokenProvider;
 import com.example.ecommerce.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
