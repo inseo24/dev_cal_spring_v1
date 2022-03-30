edb7c3c51feef35a4fb99ba6a1223b53f607f05d
choi seoin <inseo@uupfi.com>
2022-03-30 17:43:42 +0900

    :recycle: createTime, modifiedTime -> @CreationTimestamp, @UpdateTimestamp 로 변경

--- src/main/java/com/example/ecommerce/model/UserEntity.java
+++ src/main/java/com/example/ecommerce/model/UserEntity.java
@@ -5,11 +5,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import com.example.ecommerce.dto.UserUpdateDTO;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -18,6 +17,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Builder
 @NoArgsConstructor
@@ -44,16 +44,11 @@ public class UserEntity {
 	
 	private String mobileNum;
 	
-	@Column(nullable = false)
+	@CreationTimestamp
 	private LocalDateTime createdTime;
-	
-	private LocalDateTime modifiedTime;
-	
-	@PrePersist
-	public void createdTime() {
-		this.createdTime = LocalDateTime.now();
-	}
 
+	@UpdateTimestamp
+	private LocalDateTime modifiedTime;
 
 	public void updatePassword(String password) {
 		this.password = password;
