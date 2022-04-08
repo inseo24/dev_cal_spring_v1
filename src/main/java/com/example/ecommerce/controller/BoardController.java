e349435394f4053a0b4289c9e7c5de9c0395a2c7
choi seoin <inseo@uupfi.com>
2022-04-08 12:49:36 +0900

    :recycle: board dto, image dto 분리 완료 서비스 쪽 로직 수정이 꽤 많이 필요할 거 같다...

--- src/main/java/com/example/ecommerce/controller/BoardController.java
+++ src/main/java/com/example/ecommerce/controller/BoardController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.example.ecommerce.dto.board.request.CreateBoardDto;
+import com.example.ecommerce.dto.board.request.UpdateBoardDto;
+import com.example.ecommerce.dto.board.response.BoardResponseDto;
 import com.example.ecommerce.dto.image.request.CreateImageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.ecommerce.dto.board.BoardDTO;
 import com.example.ecommerce.dto.image.ImageDTO;
 import com.example.ecommerce.service.BoardService;
 
@@ -33,42 +34,40 @@ public class BoardController {
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody BoardDTO request) {
-        boardService.create(BoardDTO.toDomain(request));
+    public ResponseEntity<?> create(@RequestBody CreateBoardDto request) {
+        boardService.create(CreateBoardDto.toDomain(request));
         return ok().body(HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<?> retrieve() {
-        List<BoardDTO> response = boardService.retrieve().stream().map(BoardDTO::new).collect(Collectors.toList());
+        List<BoardResponseDto> response = boardService.retrieve().stream().map(BoardResponseDto::new).collect(Collectors.toList());
         return ok().body(response);
     }
 
     @GetMapping("/{boardId}")
     public ResponseEntity<?> retrieveById(@PathVariable Long boardId) {
-        return ok().body(boardService.retrieveById(boardId));
+        return ok().body(BoardResponseDto.builder().board(boardService.retrieveById(boardId)).build());
     }
 
-    @PutMapping("/{boardId}/image")
-    public ResponseEntity<?> updateWithImage(@AuthenticationPrincipal String userId, ImageDTO imageDTO,
-                                             @RequestPart(value = "data", required = false) BoardDTO boardDTO,
-                                             @PathVariable Long boardId) {
-        boardService.updateWithImage(BoardDTO.toDomain(boardDTO), boardId, ImageDTO.toDomain(imageDTO));
+    @PutMapping("/board/image/update")
+    public ResponseEntity<?> updateWithImage(@AuthenticationPrincipal String userId,
+                                             @RequestPart(value = "data", required = false) UpdateBoardDto boardDTO,
+                                             @RequestPart ImageDTO imageDTO) {
+        boardService.updateWithImage(userId, UpdateBoardDto.toDomain(boardDTO), ImageDTO.toDomain(imageDTO));
         return ok().body(HttpStatus.OK);
     }
 
-    @PutMapping("/{boardId}")
+    @PutMapping("/board/update")
     public ResponseEntity<?> update(@AuthenticationPrincipal String userId,
-                                    @RequestPart(value = "data", required = false) BoardDTO boardDTO,
-                                    @PathVariable Long boardId) {
-        boardService.update(userId, BoardDTO.toDomain(boardDTO), boardId);
+                                    @RequestPart(value = "data", required = false) UpdateBoardDto boardDTO) {
+        boardService.update(userId, UpdateBoardDto.toDomain(boardDTO));
         return ok().body(HttpStatus.OK);
-
     }
 
     @DeleteMapping("/{boardId}")
     public ResponseEntity<?> delete(@AuthenticationPrincipal String userId, @PathVariable Long boardId) {
-        boardService.delete(boardId, userId);
+        boardService.delete(userId, boardId);
         return ok().body(HttpStatus.OK);
     }
 }
